27fb95ba3621f8dd67af24517c40b2d9a47e19ad
TienNHM.Utop <TienNHM@utop.io>
2023-05-24 22:34:39 +0700

    using `typescript` and `npm`

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const projectName = "tiennhm.github.io";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'TienNHM',
+    title: 'Portfolio',
     tagline: 'TienNHM - Middle Fullstack Web Developer',
     favicon: 'img/favicon.ico',
 
@@ -44,13 +44,13 @@ const config = {
                     sidebarPath: require.resolve('./sidebars.js'),
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl: `https://github.com/${organizationName}/${projectName}/tree/master/docs`,
+                    editUrl: `https://github.com/${organizationName}/${projectName}/tree/master`,
                 },
                 blog: {
                     showReadingTime: true,
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl: `https://github.com/${organizationName}/${projectName}/tree/master/blog`,
+                    editUrl: `https://github.com/${organizationName}/${projectName}/tree/master`,
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
