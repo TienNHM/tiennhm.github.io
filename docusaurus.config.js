e720102afb402681426fc1ee527677f7aa4ad6b4
TienNHM.Utop <TienNHM@utop.io>
2023-06-15 23:53:59 +0700

    add `google-site-verification`

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,6 +75,15 @@ const config = {
     trailingSlash: false,
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
+    headTags: [
+        {
+            tagName: 'meta',
+            attributes: {
+                name: 'google-site-verification',
+                content: 'KahpusCmJyTWNzsOBu_IjSN9SlluR7BH6lq4SnfsFsQ'
+            }
+        }
+    ],
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
