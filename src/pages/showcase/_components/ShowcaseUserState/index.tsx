b7901cf9abf86ad58652eaba34fd1c21622701f1
TienNHM <TienNHM.it@gmail.com>
2024-01-21 12:40:22 +0700

    `[showcase]` - improve CardList, remove unused code

---
+++ src/pages/showcase/_components/ShowcaseUserState/index.tsx
@@ -0,0 +1,81 @@
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+import { ProjectTagType } from "@site/src/shared/constants/ProjectConsts";
+import { Project } from "@site/src/shared/dto/Project";
+import { Operator, readOperator } from "../ShowcaseFilterToggle";
+import { useLocation } from "@docusaurus/router";
+import { sortedProjects } from "@site/src/data/projects";
+import { useState, useEffect, useMemo } from "react";
+import { readSearchName } from "../ShowcaseSearchBar";
+import { readSearchTags } from "../ShowcaseTagSelect";
+
+export type UserState = {
+    scrollTopPosition: number;
+    focusedElementId: string | undefined;
+};
+
+export function restoreUserState(userState: UserState | null) {
+    const { scrollTopPosition, focusedElementId } = userState ?? {
+        scrollTopPosition: 0,
+        focusedElementId: undefined,
+    };
+    document.getElementById(focusedElementId)?.focus();
+    window.scrollTo({ top: scrollTopPosition });
+}
+
+export function prepareUserState(): UserState | undefined {
+    if (ExecutionEnvironment.canUseDOM) {
+        return {
+            scrollTopPosition: window.scrollY,
+            focusedElementId: document.activeElement?.id,
+        };
+    }
+
+    return undefined;
+}
+
+export function filterUsers(
+    users: Project[],
+    selectedTags: ProjectTagType[],
+    operator: Operator,
+    searchName: string | null,
+) {
+    if (searchName) {
+        // eslint-disable-next-line no-param-reassign
+        users = users.filter((user) =>
+            user.title.toLowerCase().includes(searchName.toLowerCase()),
+        );
+    }
+    if (selectedTags.length === 0) {
+        return users;
+    }
+    return users.filter((user) => {
+        if (user.tags.length === 0) {
+            return false;
+        }
+        if (operator === 'AND') {
+            return selectedTags.every((tag) => user.tags.includes(tag));
+        }
+        return selectedTags.some((tag) => user.tags.includes(tag));
+    });
+}
+
+export function useFilteredUsers() {
+    const location = useLocation<UserState>();
+    const [operator, setOperator] = useState<Operator>('OR');
+    // On SSR / first mount (hydration) no tag is selected
+    const [selectedTags, setSelectedTags] = useState<ProjectTagType[]>([]);
+    const [searchName, setSearchName] = useState<string | null>(null);
+    // Sync tags from QS to state (delayed on purpose to avoid SSR/Client
+    // hydration mismatch)
+    useEffect(() => {
+        setSelectedTags(readSearchTags(location.search));
+        setOperator(readOperator(location.search));
+        setSearchName(readSearchName(location.search));
+        restoreUserState(location.state);
+    }, [location]);
+
+    return useMemo(
+        () => filterUsers(sortedProjects, selectedTags, operator, searchName),
+        [selectedTags, operator, searchName],
+    );
+}
--- src/utils/jsUtils.ts
+++ src/utils/jsUtils.ts
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 // Inspired by https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_sortby-and-_orderby
 export function sortBy<T>(
   array: T[],
